44fc01fed5af1c3f213ce2e8e8fdc85103f48cf8
Marco Narca <narcamarco@gmail.com>
2020-06-26 17:21:04 +0800

    Initial Commit

---
+++ HelloWorld.py
@@ -0,0 +1,16 @@
+import math
+import sys
+from os import rename
+
+import requests
+
+print(sys.version)
+print(sys.executable)
+def greet(who_to_greet):
+
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+r = request.get("https://coreyms.com")
+print(r.status_code)
