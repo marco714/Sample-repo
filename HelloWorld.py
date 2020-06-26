c8ca7a5f3a7ecf311d1cd478633c3abb28fd0092
Marco Narca <narcamarco@gmail.com>
2020-06-26 17:24:53 +0800

    Removed some codes

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,13 +4,7 @@ from os import rename
 
 import requests
 
-print(sys.version)
-print(sys.executable)
-def greet(who_to_greet):
-
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
 
 r = request.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
